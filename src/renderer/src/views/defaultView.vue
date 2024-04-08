44654de9918414012b82620a7c3bd9d9847f2440
Tobias Wawak <tobiaswawak@online.de>
2024-04-08 14:13:05 +0200

    init v2

--- src/renderer/src/views/defaultView.vue
+++ src/renderer/src/views/defaultView.vue
@@ -3,10 +3,23 @@
     <v-container
       class="fill-height"
       fluid
-    />
+    >
+      <v-card
+        class="justify-center"
+        width="100%"
+        height="100%"
+        fluid
+        border
+      >
+        <template #title>
+          Das ist ein Test
+        </template>
+      </v-card>
+    </v-container>
   </GenralLayout>
 </template>
 
+
 <script setup>
-import GenralLayout from "@/layout/GeneralLayout.vue";
+import GenralLayout from "@/layout/GenralLayout.vue";
 </script>
