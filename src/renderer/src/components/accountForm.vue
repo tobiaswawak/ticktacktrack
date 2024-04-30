f9697b869fb1634d8d3cd93af542143bcc33aa8f
Tobias Wawak <tobiaswawak@online.de>
2024-04-30 14:53:04 +0200

    Formular speichern und Gleitzeit in Tabelle

--- src/renderer/src/components/accountForm.vue
+++ src/renderer/src/components/accountForm.vue
@@ -1,5 +1,3 @@
-
-
 <template>
   <div class="ma-5 ml-7">
     <v-avatar size="80">
@@ -20,12 +18,19 @@
     </v-btn>
   </div>
 
-  <v-form class="mt-5">
+  <v-form
+    @submit.prevent="saveAccount"
+    lazy-validation
+    ref="form"
+    class="mt-5"
+  >
     <v-row class="mx-5">
       <v-col cols="6">
         <v-text-field
+          v-model="firstName"
           label="Vorname"
-          type="input"
+          type="text"
+          :rules="rules"
           required
           variant="outlined"
           color="primary"
@@ -35,8 +40,10 @@
 
       <v-col cols="6">
         <v-text-field
+          v-model="lastName"
           label="Nachname"
           type="input"
+          :rules="rules"
           required
           variant="outlined"
           color="primary"
@@ -47,8 +54,10 @@
     <v-row class="mx-5">
       <v-col cols="12">
         <v-text-field
+          v-model="email"
           label="E-Mail"
           type="email"
+          :rules="rules"
           required
           variant="outlined"
           color="primary"
@@ -61,8 +70,10 @@
     <v-row class="mx-5">
       <v-col cols="6">
         <v-text-field
+          v-model="jobTitle"
           label="Jobtitel"
-          type="input"
+          type="text"
+          :rules="rules"
           required
           variant="outlined"
           color="primary"
@@ -72,8 +83,10 @@
 
       <v-col cols="6">
         <v-text-field
-          label="Arbeitsstunden"
+          v-model="workingHours"
+          label="Arbeitsstunden pro Woche"
           type="number"
+          :rules="rules"
           required
           variant="outlined"
           color="primary"
@@ -100,9 +113,45 @@
 
 
 <script setup>
-import { ref } from "vue";
+import {ref} from "vue";
+import {useSaveAccountData} from "../stores/saveAccountData.js";
+const saveAccountData = useSaveAccountData();
+const form = ref(null);
+const firstName = ref("")
+const lastName = ref("")
+const email = ref("")
+const jobTitle = ref("")
+const workingHours = ref("")
 
-const selectedImage = ref(null);
+if (saveAccountData.accountData.length > 0) {
+  const data = saveAccountData.accountData[0]; // Annahme: Es gibt nur einen Datensatz
+  firstName.value = data.firstName;
+  lastName.value = data.lastName;
+  email.value = data.email;
+  jobTitle.value = data.jobTitle;
+  workingHours.value = data.workingHours;
+}
+
+const rules = [
+  value => {
+    if (value) return true;
+    return "Bitte eintragen";
+  },
+];
+
+const saveAccount = async () => {
+    const valid = await form.value.validate();
+  if (valid.valid) {
+    saveAccountData.accountData.splice(0, 1, {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
+      jobTitle: jobTitle.value,
+      workingHours: workingHours.value
+    });
+  }
+}
+
 </script>
 
 <style scoped>
