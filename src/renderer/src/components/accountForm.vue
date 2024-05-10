5dd2404e9ede40264f76b358e44ab9061f16a035
Tobias Wawak <tobiaswawak@online.de>
2024-05-10 15:35:29 +0200

    profilePicture and defaultavatar

--- src/renderer/src/components/accountForm.vue
+++ src/renderer/src/components/accountForm.vue
@@ -1,46 +1,113 @@
 <template>
-  <div class="ma-5 ml-7">
-    <v-avatar size="80">
-      <v-img :src="userdata.image" />
+  <div class="ma-5 ml-7 mt-6">
+    <v-avatar size="75">
+      <v-img
+        v-if="userdata.image !== null"
+        :src="userdata.image"
+      />
+
+      <v-img
+        v-else
+        src="/src/images/default_Avatar.jpg"
+      />
     </v-avatar>
-    <v-btn v-if="userdata.image !== null" icon="mdi-trash-can" border @click="deleteProfileImg()" class="ml-5" />
-    <v-file-input v-else v-model="image" accept="image/*" color="primary" variant="outlined" label="Profilbild auswählen">
+    <v-btn
+      v-if="userdata.image !== null"
+      icon="mdi-trash-can"
+      border
+      @click="deleteProfileImg()"
+      class="ml-6"
+    />
+    <v-file-input
+      v-else
+      v-model="image"
+      accept="image/*"
+      color="primary"
+      variant="outlined"
+      label="Profilbild auswählen"
+      class="mt-6"
+    >
       <template #append>
-        <v-btn border @click="saveImg()">
+        <v-btn
+          border
+          @click="saveImg()"
+          class="mr-3"
+        >
           <v-icon>mdi-content-save</v-icon>
         </v-btn>
       </template>
     </v-file-input>
   </div>
 
-  <v-form lazy-validation ref="form" class="mt-5">
+  <v-form
+    lazy-validation
+    ref="form"
+    class="mt-5"
+  >
     <v-row class="mx-5">
       <v-col cols="6">
-        <v-text-field v-model="userdata.firstName" label="Vorname" type="text" :rules="rules" required variant="outlined"
-          color="primary" prepend-icon="mdi-account-edit-outline" />
+        <v-text-field
+          v-model="userdata.firstName"
+          label="Vorname"
+          type="text"
+          :rules="rules"
+          required
+          variant="outlined"
+          color="primary"
+          prepend-icon="mdi-account-edit-outline"
+        />
       </v-col>
 
       <v-col cols="6">
-        <v-text-field v-model="userdata.lastName" label="Nachname" type="input" :rules="rules" required variant="outlined"
-          color="primary" />
+        <v-text-field
+          v-model="userdata.lastName"
+          label="Nachname"
+          type="input"
+          :rules="rules"
+          required
+          variant="outlined"
+          color="primary"
+        />
       </v-col>
     </v-row>
 
     <v-row class="mx-5">
       <v-col cols="6">
-        <v-text-field v-model="userdata.jobTitle" label="Jobtitel" type="text" :rules="rules" required variant="outlined"
-          color="primary" prepend-icon="mdi-briefcase-outline" />
+        <v-text-field
+          v-model="userdata.jobTitle"
+          label="Jobtitel"
+          type="text"
+          :rules="rules"
+          required
+          variant="outlined"
+          color="primary"
+          prepend-icon="mdi-briefcase-outline"
+        />
       </v-col>
 
       <v-col cols="6">
-        <v-text-field v-model="userdata.workingHours" label="Arbeitsstunden pro Woche" type="number" :rules="rules"
-          required variant="outlined" color="primary" />
+        <v-text-field
+          v-model="userdata.workingHours"
+          label="Arbeitsstunden pro Woche"
+          type="number"
+          :rules="rules"
+          required
+          variant="outlined"
+          color="primary"
+        />
       </v-col>
     </v-row>
 
-    <v-row justify="center" class="mb-5">
+    <v-row
+      justify="center"
+      class="mb-5"
+    >
       <v-col cols="auto">
-        <v-btn border @click="saveUserData" color="primary">
+        <v-btn
+          border
+          @click="saveUserData"
+          color="primary"
+        >
           Speichern
         </v-btn>
       </v-col>
@@ -79,6 +146,8 @@ const saveImg = () => {
   userdata.value.image = URL.createObjectURL(image.value)
   image.value = null
 }
+
+
 
 </script>
 
